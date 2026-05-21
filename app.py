13ff99cf80a4c4405b0897db5e61c332ddfb6f8c
medicalnetalbania <medicalnet.al@gmail.com>
2026-05-21 22:01:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def webhook_al():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
